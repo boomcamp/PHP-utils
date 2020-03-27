88eab3b3142fa04cdb87679d281480ead8cf2c8e
Jino Lacson <jino.lacson@boom.camp>
2020-03-27 17:02:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ echo USER='yourgithubusername' >.env
 echo PASSWORD='yourgihubpassword' >> .env 
 echo REPO='boomcamp/PHP-ToyProblems-and-Activity-submissions' >> .env
 
+# Create containers for cloned submissions and screenshots
+mkdir pulls/
+mkdir captures/
+
 # Configure virtual environment
 pipenv install
 pipenv shell
